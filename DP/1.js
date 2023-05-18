0524149e191067b8e41be991b93c0781839f0d9c
hongzi <66042404+810331109@users.noreply.github.com>
2023-05-18 21:18:10 +0800

    Add files via upload

---
+++ DP/1.js
@@ -0,0 +1,11 @@
+<link rel="shortcut icon" type="image/ico" href="/DP/favicon.ico">
+<link rel="stylesheet" href="/DP/css/Maoplayer.css">
+<script type="text/javascript" src="/DP/js/hz.js"></script>
+<script type="text/javascript" src="/DP/js/aes.js"></script>
+<script type="text/javascript" src="/DP/js/jquery.min.js"></script>
+<script type="text/javascript" src="/DP/js/play.js"></script>
+<script type="text/javascript" src="/DP/js/Mao.js"></script>
+<script type="text/javascript" src="/DP/js/Maoplay.js"></script>
+<script type="text/javascript" src="/DP/js/hls.min.js"></script>
+<script type="text/javascript" src="/DP/js/flv.min.js"></script>
+<script type="text/javascript" src="/DP/js/layer.js"></script>
