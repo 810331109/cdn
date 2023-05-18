ba8b93a71484656ef09870d369eacfde4884818d
hongzi <66042404+810331109@users.noreply.github.com>
2023-05-18 21:13:23 +0800

    Add files via upload

--- DP/@.js
+++ DP/@.js
@@ -1,11 +1,11 @@
 <link rel="shortcut icon" type="image/ico" href="favicon.ico">
-<link rel="stylesheet" href="/css/Maoplayer.css">
-<script type="text/javascript" src="/js/hz.js"></script>
-<script type="text/javascript" src="/js/aes.js"></script>
-<script type="text/javascript" src="/js/jquery.min.js"></script>
-<script type="text/javascript" src="/js/play.js"></script>
-<script type="text/javascript" src="/js/Mao.js"></script>
-<script type="text/javascript" src="/js/Maoplay.js"></script>
-<script type="text/javascript" src="/js/hls.min.js"></script>
-<script type="text/javascript" src="/js/flv.min.js"></script>
-<script type="text/javascript" src="/js/layer.js"></script>
+<link rel="stylesheet" href="DP/css/Maoplayer.css">
+<script type="text/javascript" src="DP/js/hz.js"></script>
+<script type="text/javascript" src="DP/js/aes.js"></script>
+<script type="text/javascript" src="DP/js/jquery.min.js"></script>
+<script type="text/javascript" src="DP/js/play.js"></script>
+<script type="text/javascript" src="DP/js/Mao.js"></script>
+<script type="text/javascript" src="DP/js/Maoplay.js"></script>
+<script type="text/javascript" src="DP/js/hls.min.js"></script>
+<script type="text/javascript" src="DP/js/flv.min.js"></script>
+<script type="text/javascript" src="DP/js/layer.js"></script>
